bf91b05d5f977f5159443000f3c8cfa5b2b763ee
Harika K <harika.kasireddy@gmail.com>
2016-10-20 18:04:18 -0400

    Add files via upload

--- word.js
+++ word.js
@@ -1,22 +1,3 @@
-
-//list of letters object
-//a boolean that says if word is guessed or not
-// var LetterObjects = [];
-// var guessed = false;
-//display text function
-// loop through letterobject array
-// for each letterobject get the text
-// concatenate text for each letter object
-// return concatendated string
-
-// actions methods
-// isGuessed methos that returns guessed
-// if (secret word.isGuessed) {
-// 	gameover}
- 
-
-
-
 var letter = require('letter.js');
 
 function Word(target) {
